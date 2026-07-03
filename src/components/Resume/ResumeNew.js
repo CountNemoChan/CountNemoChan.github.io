4a9a26b951bd83aa9bfd3fe833ff40a8d1dbfc13
CountNemoChan <liheng.chen@connect.ust.hk>
2026-07-03 17:16:42 +0800

    20260703

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Li-Heng_Chen_CV.pdf";
+import pdf from "../../Assets/CHEN_Liheng _CV_phd.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
